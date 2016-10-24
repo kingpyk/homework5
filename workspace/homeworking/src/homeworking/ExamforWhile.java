ed93419fcea9d9efd0d95bfba442b033a1ec060f
kingpyk <kingpyk@naver.com>
2016-10-25 01:45:59 +0900

    홀수단/짝수단 출력형태 수정

--- workspace/homeworking/src/homeworking/ExamforWhile.java
+++ workspace/homeworking/src/homeworking/ExamforWhile.java
@@ -135,18 +135,20 @@ void ggd2(){
 	String s = e.next();
 	if(s.equals("E"))
 	{
-		for(int i=2; i<10;i+=2){
-			for(int j=1;j<10;j++){
-				System.out.println(i+"*"+j+"="+i*j);
+		for(int i=1; i<10;i++){
+			for(int j=2;j<10;j+=2){
+				System.out.println(j+"*"+i+"="+i*j+"\t");
 			}
+			System.out.println();
 		}
 	}
 	if(s.equals("O"))
 	{
-		for(int i=1; i<10;i+=2){
-			for(int j=1;j<10;j++){
-				System.out.println(i+"*"+j+"="+i*j);
+		for(int i=1; i<10;i++){
+			for(int j=1;j<10;j+=2){
+				System.out.println(j+"*"+i+"="+i*j+"\t");
 			}
+			System.out.println();
 		}
 	}
 	if(s.equals("Q"))
